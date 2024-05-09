f1b313f9c58eef6ec71e607eeeb35722200faf7e
hugorebel1998 <hugorebel1998@gmail.com>
2024-05-09 13:23:21 -0600

    MUI: Se implementa configuración oara obtener estilos

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,7 +1,10 @@
 import { AppRouter } from "./router/AppRouter"
+import { AppTheme } from "./themes"
 
 export const JournalApp = () => {
     return (
-        <AppRouter />
+        <AppTheme>
+            <AppRouter />
+        </AppTheme>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ import '@fontsource/roboto/700.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <JournalApp />
+        <JournalApp />
     </BrowserRouter>
   </React.StrictMode>,
 )
